a7cc296b830cb69e1e919d6cc25669426ac72dcf
pranotithaware <thaware.pranoti@praxislogica.com>
2019-04-03 11:54:36 +0530

    3 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 //import { HelloComponent } from './hello.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, HeroesComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
